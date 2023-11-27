05eeab3d0e80839022ea315c928fe3043fc22939
santhasurya <727722euit164@skcet.ac.in>
2023-11-27 09:49:28 -0800

    commit

--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -1,20 +1,4 @@
-// Logout.js
 
-// import React from 'react';
-
-// const Logout = () => {
-//   window.localStorage.removeItem('user'); 
-//   window.location.href = '/login'; 
-
-//   return (
-//     <div>
-//       Logging out...
-//       {/* You may include a loading spinner or a message here */}
-//     </div>
-//   );
-// };
-
-// export default Logout;
 
 
 // Logout.js
--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -1,29 +1,6 @@
 
 
 
-// import { createBrowserRouter } from "react-router-dom";
-// import SignUp2 from "./SignUp2";
-// import Loginpage2 from "./Loginpage2";
-// import Front from "./Front";
-// import Home from "./Home";
-// import Products from "./Products";
-// import Contact from "./Contact";
-// import Services from "./Services";
-// import AboutUs from "./AboutUs";
-// const router = createBrowserRouter([
-//   { path: "/", element: <Home /> },
-//   { path: "/signup", element: <SignUp2 /> },
-//   { path: "/login", element: <Loginpage2 /> },
-//   { path: "/front", element: <Front /> },
-//   { path: "/products", element: <Products /> },
-//   { path: "/contact", element: <Contact /> },
-//   { path: "/services", element: <Services /> },
-//   { path: "/aboutus", element: <AboutUs /> },
-// ]);
-// export default router;
-
-
-
 import { createBrowserRouter } from "react-router-dom";
 import SignUp2 from "./SignUp2";
 import Loginpage2 from "./Loginpage2";
@@ -51,31 +28,3 @@ const router = createBrowserRouter([
 export default router;
 
 
-// import { createBrowserRouter } from "react-router-dom";
-// import SignUp2 from "./SignUp2";
-// import Loginpage2 from "./Loginpage2";
-// import Front from "./Front";
-// import Home from "./Home";
-// // import Products from "./Products";
-// import Contact from "./Contact";
-// import GetProducts from './GetProducts';
-// import Services from "./Services";
-// import AboutUs from "./AboutUs";
-// //import Product from "./Product";
-// import Cartitems from "./Cartitems";
-
-// const router = createBrowserRouter([
-//   { path: "/", element: <Home/> },
-//   { path: "/signup", element: <SignUp2 /> },
-//   { path: "/login", element: <Loginpage2 /> },
-//   { path: "/home", element: <Home /> },
-//   //{ path: "/products", element: <Products /> },
-//   { path: "/contact", element: <Contact /> },
-//   { path: "/products", element: <GetProducts /> },
-//   { path: "/services", element: <Services /> },
-//   { path: "/aboutus", element: <AboutUs /> },
-//   //{path : "/product", element: <Product/>},
-//   {path : "/cartItems", element: <Cartitems/>}
-
-// ]);
-// export default router;
